4b738c1e3d3214e2e056a61d91142926cbb4148b
Hossein Khosravipour <hossein.kh7935@gmail.com>
2024-01-09 19:19:56 +0330

    commit

--- going_modular/engine.py
+++ going_modular/engine.py
@@ -14,54 +14,6 @@ def one_step_train(model,
     model.train()
 
     train_loss, train_iou = 0, 0
-    # for batch, (X, y) in enumerate(train_dataloader):
-        
-        # X, y = X.to(device), y.to(device)
-        # y = torch.where(y == -1, torch.tensor(0), y)
-
-        # #calculate iou
-        # # Assuming your target mask is 'target' with shape (batch_size, 1, width, height)
-        # logits = model(X)  # Example logits
-        # # logits = logits.to(torch.int32)
-
-        # # Assuming your actual target size is (2, 1, 256, 256)
-        # target = y.to(torch.int64)  # Example target mask
-
-        # # Convert target to one-hot encoded tensor
-        # target_onehot = torch.zeros_like(logits)
-        # target_onehot.scatter_(1, target, 1)
-
-        # # Apply softmax to get probabilities along the channel dimension
-        # probs = F.softmax(logits, dim=1)
-
-        # # Convert probabilities to one-hot encoded predictions
-        # _, predicted = torch.max(probs, 1)
-
-        # # Convert predicted to one-hot encoded tensor
-        # predicted_onehot = torch.zeros_like(logits)
-        # predicted_onehot.scatter_(1, predicted.unsqueeze(1), 1)
-
-        # # Compute IoU
-        # iou = intersection_over_union_multiclass(predicted_onehot, target_onehot)
-        # train_iou += iou
-
-
-        # #calculate loss
-        # y = y.reshape(-1).long()
-
-        # y_pred = model(X)
-        # # Reshape the target to (batch_size * height * width)
-        # y_pred = y_pred.permute(0, 2, 3, 1).contiguous().view(-1, configs.NUM_CLASSES)
-
-        # loss = loss_fn(y_pred, y)
-        # train_loss += loss.item()
-        # # print(f'loss in {batch} is {loss}')
-        # # print(f'loss in {batch} is {loss}')
-
-        # optimizer.zero_grad()
-
-        # loss.backward()
-        # optimizer.step()
 
     for i, (inputs, targets) in enumerate(train_dataloader):
 
@@ -94,6 +46,9 @@ def one_step_train(model,
         batch_iou = compute_iou(predictions, targets, num_classes)
 
         train_iou += batch_iou
+
+        if i % 2 == 0:
+            print(f'train_mode i is: {i}')
 
     train_loss = train_loss/len(train_dataloader)
     tain_iou = train_iou/len(train_dataloader)
@@ -137,7 +92,7 @@ def one_step_test(model,
             test_iou += batch_iou
             
             if i % 2 == 0:
-                print(i)
+                print(f'test_mode i is: {i}')
 
     test_iou = test_iou / len(test_dataloader)
     test_loss = test_loss/ len(test_dataloader)
@@ -182,9 +137,9 @@ def train(model,
         print(
           f"Epoch: {epoch+1} | "
           f"train_loss: {train_loss:.4f} | "
-          f"train_iou: {train_iou:.4f} | "
+          f"train_iou: {train_iou*100:.2f} | "
           f"test_loss: {test_loss:.4f} | "
-          f"test_iou: {test_iou:.4f}"
+          f"test_iou: {test_iou*100:.2f}"
         )
         
     return results
